e2d981609c96d4fa4edce0bed64b5ec47284eaf8
Tomas Racek <tom@krab1k.net>
2013-10-17 16:35:39 +0200

    Rewrite if else if structure to switch

--- neemp.c
+++ neemp.c
@@ -28,57 +28,53 @@ int main(int argc, char **argv) {
 	parse_options(argc, argv);
 	check_settings();
 
-	if(s.mode == MODE_PARAMS) {
-		load_molecules();
-		load_charges();
-		preprocess_molecules();
+	load_molecules();
+	preprocess_molecules();
 
-		ts_info();
+	switch(s.mode) {
+		case MODE_PARAMS: {
+			ts_info();
 
-		struct subset full;
-		b_init(&full.molecules, ts.molecules_count);
-		b_set_all(&full.molecules);
-		find_the_best_parameters_for_subset(&full);
+			struct subset full;
+			b_init(&full.molecules, ts.molecules_count);
+			b_set_all(&full.molecules);
+			find_the_best_parameters_for_subset(&full);
 
-		if(s.chg_stats_out_file[0] != '\0')
-			output_charges_stats(&full);
+			if(s.chg_stats_out_file[0] != '\0')
+				output_charges_stats(&full);
 
-		if(s.par_out_file[0] != '\0')
-			output_parameters(&full);
+			if(s.par_out_file[0] != '\0')
+				output_parameters(&full);
 
-		print_results(&full);
+			print_results(&full);
 
-		ss_destroy(&full);
+			ss_destroy(&full);
+			break;
+		}
+		case MODE_CHARGES: {
+			struct subset full;
+			b_init(&full.molecules, ts.molecules_count);
+			b_set_all(&full.molecules);
 
-	} else if(s.mode == MODE_CHARGES) {
-		load_molecules();
+			full.kappa_data_count = 1;
+			full.data = (struct kappa_data *) calloc(1, sizeof(struct kappa_data));
+			full.best = &full.data[0];
+			kd_init(full.best);
 
-		preprocess_molecules();
+			load_parameters(full.best);
 
-		struct subset full;
-		b_init(&full.molecules, ts.molecules_count);
-		b_set_all(&full.molecules);
+			calculate_charges(&full, full.best);
+			calculate_statistics(&full, full.best);
 
-		full.kappa_data_count = 1;
-		full.data = (struct kappa_data *) calloc(1, sizeof(struct kappa_data));
-		full.best = &full.data[0];
-		kd_init(full.best);
+			print_results(&full);
+			output_charges(&full);
 
-		load_parameters(full.best);
-
-		calculate_charges(&full, full.best);
-		calculate_statistics(&full, full.best);
-
-		print_results(&full);
-
-		output_charges(&full);
-
-		ss_destroy(&full);
-
-	} else if(s.mode == MODE_INFO) {
-		load_molecules();
-		preprocess_molecules();
-		ts_info();
+			ss_destroy(&full);
+			break;
+		}
+		case MODE_INFO:
+			ts_info();
+			break;
 	}
 
 	ts_destroy();
