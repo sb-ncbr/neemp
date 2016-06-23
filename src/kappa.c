80f2d4fabc22a505e848c901e3a67a2ae4b0621b
Tomas Racek <tom@krab1k.net>
2016-06-23 13:32:02 +0200

    Add missing limitation on number of threads
    Full scan was not limited to the s.max_threads. Fixed now.

--- src/kappa.c
+++ src/kappa.c
@@ -45,7 +45,7 @@ static void full_scan(struct subset * const ss) {
 
 	assert(ss != NULL);
 
-	#pragma omp parallel for
+	#pragma omp parallel for num_threads(s.max_threads)
 	for(int i = 0; i < ss->kappa_data_count - 1; i++) {
 		ss->data[i].kappa = i * s.full_scan_precision;
 		perform_calculations(ss, &ss->data[i]);
