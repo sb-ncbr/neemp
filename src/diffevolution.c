286c7c9f4e5db1051f3b4c6b91be1d19bc51f0d6
Jana Pazurikova <pazurikova@ics.muni.cz>
2016-06-24 13:47:46 +0200

    Change default values for iterations in DE

--- src/diffevolution.c
+++ src/diffevolution.c
@@ -98,7 +98,7 @@ void run_diff_evolution(struct subset * const ss) {
 	
 	/* Minimize the best of the population once more */
 	if (s.polish > 2) {
-			minimize_locally(so_far_best, 500);
+			minimize_locally(so_far_best, 1000);
 			calculate_charges(ss, so_far_best);
 			calculate_statistics(ss, so_far_best);
 			kd_print_results(so_far_best);
@@ -200,7 +200,7 @@ void run_diff_evolution(struct subset * const ss) {
 
 	/* Minimize the result */
 	if (s.polish > 0)
-		minimize_locally(so_far_best, 500);
+		minimize_locally(so_far_best, 2000);
 
 	/* Tidying up and printing */
 	kd_destroy(trial);
